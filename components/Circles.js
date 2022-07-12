4e1b4484ec86cd9a6d0629e62496568b5ab3eccd
Aniket <rajeshwarkumar9204@gmail.com>
2022-07-12 09:03:59 +0530

    change the position of first circle

--- components/Circles.js
+++ components/Circles.js
@@ -2,8 +2,8 @@ const Circles = () => {
   return (
     <>
       <div className="absolute w-full h-full top-0 left-0 overflow-x-hidden -z-10">
-        <div className="absolute h-[300px] w-[300px] bg-primary top-10 left-5 lg:left-16 rounded-full -translate-y-1/2 -z-10 blur-[250px]"></div>
-        <div className="absolute h-[300px] w-[300px] bg-purple-600 top-[50vh] right-0 lg:right-16 translate-x-1/2 rounded-full -z-10 blur-[250px]"></div>
+        <div className="absolute h-[300px] w-[300px] bg-primary top-10 left-5 lg:left-16 rounded-full -translate-y-1/2 -translate-x-3/4 md:-translate-x-0 -z-10 blur-[250px]"></div>
+        <div className="absolute h-[300px] w-[300px] bg-purple-600 top-[50vh] right-0 lg:right-16 translate-x-3/4 md:translate-x-1/2 rounded-full -z-10 blur-[250px]"></div>
         <div className="absolute h-[300px] w-[300px] bg-blue-500 top-full left-0 -translate-y-full rounded-full -z-10 blur-[150px]"></div>
       </div>
     </>
