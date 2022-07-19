c625e8fc83d0a84b9945afe4322e1ab291ab6481
Aniket <rajeshwarkumar9204@gmail.com>
2022-07-20 00:31:41 +0530

    add missing data foler

---
+++ data/testimonial.js
@@ -0,0 +1,29 @@
+const testimonial = [
+  {
+    name: "Aniket Kumar",
+    feedback:
+      "A number of practice sessions along with the daily group discussions and perfect grammar explaination turned to be very helpful in my fluency in English and I can see visibile progress in me. I can also not forget to mention the personality development lectures that was the cherry on cake. Thanks to PACE Institute!",
+  },
+  {
+    name: "Manisha Kumari",
+    feedback:
+      "I think PACE is the best institute in India for spoken English, grammar, and the most important Personality Development courses... I am seen myself changed after joining this institution.",
+  },
+  {
+    name: "Kanishk Raj Singh",
+    feedback:
+      "I am proud to be a first batch student of this institute when madam leaves BBC. MADAM ko mera pranam. Hope madam still remember me.",
+  },
+  {
+    name: "Smriti Rani",
+    feedback:
+      "It's a very good institute for Personality Development and spoken. I went there and learned many things. I loved it.",
+  },
+  {
+    name: "Abhishek Kumar",
+    feedback:
+      "It is the best institute. Sir creates a good environment to learn English &amp; many other activities that give benefit in GD and PI",
+  },
+];
+
+export default testimonial;
