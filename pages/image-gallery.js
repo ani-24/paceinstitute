7d9fe3698dfd6eb487ca5708a26857489a8b668e
Aniket <rajeshwarkumar9204@gmail.com>
2022-07-20 11:15:35 +0530

    add some meta description

--- pages/image-gallery.js
+++ pages/image-gallery.js
@@ -66,34 +66,43 @@ const ImageGallery = () => {
     setSIndex(sIndex);
   };
   return (
-    <div className="container grid gap-12 lg:grid-cols-2 mt-12">
-      {images.map((img, index) => (
-        <div
-          className="flex justify-center items-center max-w-[500px] mx-auto"
-          key={index}
-        >
+    <>
+      <Head>
+        <title>Pace Institute - Image Gallery - Images of Pace Institute</title>
+        <meta
+          name="description"
+          content="PACE Institute is the leading Institute for English language and personality development in the eastern part of India. It has won many awards for its expert training programs in spoken English, communication skill, interview training, etc. It has mentored thousands of students and helped them to be the part of this global world. Many students have successfully competed in the competitions and many of them have settled abroad. Students interested to go to America and Australia have an added advantage to join PACE Institute. The regional head office is situated in Kankarbagh Patna. Learning English language requires a good environment an practice of speaking writing listening and reading.  At PACE, students are provided good environment of learning."
+        />
+      </Head>
+      <div className="container grid gap-12 lg:grid-cols-2 mt-12">
+        {images.map((img, index) => (
           <div
-            className="border-b-8 border-primary"
-            onClick={() => {
-              lightBoxHandler(true, index);
-            }}
+            className="flex justify-center items-center max-w-[500px] mx-auto"
+            key={index}
           >
-            <img src={img.image} alt={img.alt} loading="lazy" />
+            <div
+              className="border-b-8 border-primary"
+              onClick={() => {
+                lightBoxHandler(true, index);
+              }}
+            >
+              <img src={img.image} alt={img.alt} loading="lazy" />
+            </div>
           </div>
-        </div>
-      ))}
-      <LightBox
-        state={toggle}
-        event={lightBoxHandler}
-        data={images}
-        imageWidth="60vw"
-        imageHeight="70vh"
-        thumbnailHeight={50}
-        thumbnailWidth={50}
-        setImageIndex={setSIndex}
-        imageIndex={sIndex}
-      />
-    </div>
+        ))}
+        <LightBox
+          state={toggle}
+          event={lightBoxHandler}
+          data={images}
+          imageWidth="60vw"
+          imageHeight="70vh"
+          thumbnailHeight={50}
+          thumbnailWidth={50}
+          setImageIndex={setSIndex}
+          imageIndex={sIndex}
+        />
+      </div>
+    </>
   );
 };
 
--- pages/video-gallery.js
+++ pages/video-gallery.js
@@ -3,11 +3,20 @@ import videos from "../data/videos";
 
 const VideoGallery = () => {
   return (
-    <div className="container grid md:grid-cols-2 lg:grid-cols-3 gap-10">
-      {videos.map((video) => (
-        <Video video={video} key={video._id} />
-      ))}
-    </div>
+    <>
+      <Head>
+        <title>Pace Institute - Watch our videos and lectures</title>
+        <meta
+          name="description"
+          content="PACE Institute is the leading Institute for English language and personality development in the eastern part of India. It has won many awards for its expert training programs in spoken English, communication skill, interview training, etc. It has mentored thousands of students and helped them to be the part of this global world. Many students have successfully competed in the competitions and many of them have settled abroad. Students interested to go to America and Australia have an added advantage to join PACE Institute. The regional head office is situated in Kankarbagh Patna. Learning English language requires a good environment an practice of speaking writing listening and reading. At PACE, students are provided good environment of learning. Watch some of our demo videos and lectures"
+        />
+      </Head>
+      <div className="container grid md:grid-cols-2 lg:grid-cols-3 gap-10">
+        {videos.map((video) => (
+          <Video video={video} key={video._id} />
+        ))}
+      </div>
+    </>
   );
 };
 
